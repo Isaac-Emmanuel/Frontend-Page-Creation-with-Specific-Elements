66cda0c18322ff81c37e5142e98c7958390456e4
Isaac-Emmanuel <66456090+Isaac-Emmanuel@users.noreply.github.com>
2023-09-08 11:53:58 +0100

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the current date
+    // Get the current date and time in UTC
     const currentDate = new Date();
 
     // Get the day of the week as a string (e.g., "Monday")
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
 
+    // Format the current UTC time as a string
+    const currentUTCTime = currentDate.toUTCString();
+
     // Update the elements with data-testid attributes
     document.querySelector("[data-testid='currentDayOfTheWeek']").textContent = currentDayOfWeek;
-    document.querySelector("[data-testid='currentUTCTime']").textContent = currentDate.getTime();
+    document.querySelector("[data-testid='currentUTCTime']").textContent = currentUTCTime;
 });
